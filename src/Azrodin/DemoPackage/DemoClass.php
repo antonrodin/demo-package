2524b9e39292845b9bd85cabcf5d3ce89a04323e
Anton Zekeriev Rodin <antonzekeriev@gmail.com>
2014-03-01 14:53:45 +0100

    First Commit

---
+++ src/Azrodin/DemoPackage/DemoClass.php
@@ -0,0 +1,11 @@
+<?php namespace Azrodin\DemoPackage;
+
+/**
+ * Demo Class
+ * @author Anton Zekeriev Rodin
+ */
+class DemoClass {
+    static public function foo() {
+        return "Bar";
+    }
+}
